97a11a12def474bb6f308dc0eba5d46814fa5e10
liliangabc <ll917274996@live.com>
2021-03-25 21:59:09 +0800

    完善步骤条组件

--- packages/xview-ui/src/components/steps/index.js
+++ packages/xview-ui/src/components/steps/index.js
@@ -1,7 +1,5 @@
 /** @typedef {import('vue').ComponentOptions} ComponentOptions */
 
-import { getChildComponents } from '../../utils'
-
 const N = Number
 const S = String
 const B = Boolean
@@ -19,21 +17,26 @@ export const Step = {
   data () {
     return { statusObj: {} }
   },
+  mounted () {
+    const { xSteps } = this
+    xSteps.addItem(this)
+    this.$once('hook:beforeDestroy', () => {
+      Steps.removeItem(this)
+    })
+  },
   render (h) {
     const prefixCls = 'x-step'
     const { icon, statusObj } = this
-    const { index, status } = statusObj
+    const { status } = statusObj
     const iconVNode = this.$slots.icon || (icon && h('i', { class: icon }))
     const statusIcon = ({ success: 'x-icon-check', error: 'x-icon-close' })[status]
     const desc = this.$slots.description || this.description
     return h('li', {
       class: [prefixCls, `is-${status}`]
     }, [
-      h('div', {
-        class: `${prefixCls}_head`
-      }, [
+      h('div', { class: `${prefixCls}_head` }, [
         h('div', { class: [`${prefixCls}_icon`, { 'is-text': !iconVNode }] }, [
-          iconVNode || (statusIcon && h('i', { class: statusIcon })) || h('span', index + 1)
+          iconVNode || (statusIcon && h('i', { class: statusIcon })) || h('span', statusObj.index + 1)
         ]),
         h('div', { class: `${prefixCls}_line` })
       ]),
@@ -60,29 +63,46 @@ export const Steps = {
     alignCenter: B,
     simple: B
   },
-  watch: {
-    active: {
-      immediate: true,
-      handler: 'updateItemStatus'
-    }
+  data () {
+    return { items: [] }
+  },
+  mounted () {
+    this.$once('hook:beforeDestroy', this.$watch(
+      () => [this.active, this.items.length],
+      () => {
+        this.$nextTick(() => {
+          const { active, items } = this
+          items.forEach((_, i) => {
+            _.statusObj = {
+              index: i,
+              status: _.status || (i === active ? this.processStatus : i < active ? this.finishStatus : 'wait')
+            }
+          })
+        })
+      },
+      { immediate: true }
+    ))
   },
   methods: {
-    updateItemStatus () {
-      const { active } = this
-      this.$nextTick(() => {
-        getChildComponents(this, 'XStep').forEach((_, i) => {
-          _.statusObj = {
-            index: i,
-            status: _.status || (i === active ? this.processStatus : i < active ? this.finishStatus : 'wait')
-          }
-        })
-      })
+    addItem (item) {
+      this.items.push(item)
+    },
+    removeItem (item) {
+      this.items.splice(this.items.indexOf(item), 1)
     }
   },
   render (h) {
     const prefixCls = 'x-steps'
+    const { simple } = this
     return h('ul', {
-      class: [prefixCls, `${prefixCls}_${this.direction}`]
+      class: [
+        prefixCls,
+        !simple && `${prefixCls}_${this.direction}`,
+        {
+          'is-simple': simple,
+          'is-center': this.alignCenter
+        }
+      ]
     }, this.$slots.default)
   }
 }
