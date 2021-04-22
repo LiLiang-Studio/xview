f1c93a0bf8047b723ce39a4d95760693cf94179f
liliangabc <ll917274996@live.com>
2021-04-22 05:00:18 +0000

    Deploying to master from  @ 51b14286ee2422a259da80370a5dbb6c65ebfec9 🚀

--- packages/xview-ui/src/components/checkbox/utils.js
+++
@@ -1,7 +0,0 @@
-export const getSize = vm => {
-  return vm.size || (vm.xCheckboxGroup || {}).size || (vm.xForm || {}).size || (vm.$XVIEW || {}).size
-}
-
-export const isDisabled = vm => {
-  return vm.disabled || (vm.xCheckboxGroup || {}).disabled || (vm.xForm || {}).disabled
-}
